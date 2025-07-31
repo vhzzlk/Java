4358ca6853ff13629354cc6c5aef30ba008fe877
vhzzlk <vhzzlk@gmail.com>
2025-07-31 10:45:22 -0300

    Commit

--- src/A1/Main.java
+++ src/A1/Main.java
@@ -5,13 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Pessoa p1 = new Pessoa();
+        Pessoa p1 = new Pessoa("Danilo", "Masculino", 20, 70.5, 1.75);
 
-        p1.nome = "Danilo";
-        p1.genero = "Masculino";
-        p1.idade = 20;
-        p1.peso = 70.5;
-        p1.altura = 1.75;
+
         System.out.println(p1);
         System.out.printf("IMC: %.2f", p1.imc());
 
--- src/A1/Pessoa.java
+++ src/A1/Pessoa.java
@@ -8,6 +8,14 @@ public class Pessoa {
     protected double peso;
     protected double altura;
 
+    public Pessoa(String nome, String genero, int idade, double peso, double altura) {
+        this.nome = nome;
+        this.genero = genero;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
     @Override
     public String toString() {
         return "Nome: " + nome + "\nGenero: " + genero + "\nIdade: " + idade + "\nPeso: " + peso + "\nAltura: " + altura;
